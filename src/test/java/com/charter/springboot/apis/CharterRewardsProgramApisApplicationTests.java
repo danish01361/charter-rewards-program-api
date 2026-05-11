80f21984858dbb625f338a14b55b84f5043c4064
md imran <bob@gmail.com>
2026-05-11 15:16:51 +0530

    Initial Spring Boot rewards assignment

---
+++ src/test/java/com/charter/springboot/apis/CharterRewardsProgramApisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.charter.springboot.apis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CharterRewardsProgramApisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
